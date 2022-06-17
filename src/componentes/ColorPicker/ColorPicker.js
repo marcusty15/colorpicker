2604215231aa9edc0f9eb01c63824e7e1bbb6b58
marcusty15 <marco.troconis15@gmail.com>
2022-06-17 19:00:11 -0400

    primer commit

---
+++ src/componentes/ColorPicker/ColorPicker.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import '../../componentes/ColorPicker/ColorPicker.css'
+
+
+
+const ColorPicker = () => {
+    const [color, setColor] = useState("#fff");
+    console.log(color)
+    const changeColor = (e) =>{
+       setColor(e.target.value)
+    }
+    
+   
+   
+  return (
+    <>  
+        <div className='colorp'>
+        <div id='entrada'className='shadow'>
+        <h2>Color Picker</h2>
+        <div id='inputs'>
+        <style>{' .colorp {background-color: '+color+';}' }</style>
+        <input className='m-' type="color" value={color} onChange={(e)=>{changeColor(e)}}/>
+        <input className='' type='text' value={color} onChange={(e)=>{changeColor(e)}}/>
+        </div>
+        </div>
+        </div>
+    </>
+  )
+}
+
+export default ColorPicker
+
+
+
+
+
